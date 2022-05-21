ba8f4ec55c51d42fb37af035a848315ff675bf01
SAMY-MOHSEN-111 <11410120200220@stud.cu.edu.eg>
2022-05-21 19:39:47 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '554d80e9-05ed-4bd0-b511-ef1da88a91a4';
+export const MODAL_CLOSE_SEC = 2.5;
